263d6f53a01945cc69b4e9b8c0eca1969d534a0b
Carlo Cervellin <carlocervellin@gmail.com>
2019-01-22 22:48:16 +0100

    ✨ Actually routing requests

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+const OAuth = require('oauth');
+const url = require('url');
 
 module.exports = (req, res) => {
-	const OAuth = require('oauth');
-	
 	const header = {
 		'Yahoo-App-Id': process.env.YAHOO_APP_ID
 	};
+	const requestOrigin = req.headers.origin;
+	const { path } = url.parse(req.url);
 	const request = new OAuth.OAuth(
 		null,
 		null,
@@ -18,7 +19,7 @@ module.exports = (req, res) => {
 		header
 	);
 	request.get(
-		'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json',
+		'https://weather-ydn-yql.media.yahoo.com' + path,
 		null,
 		null,
 		function(err, data, result) {
