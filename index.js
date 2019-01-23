f8e49ed7f887a741cfd4930c21ebd3a0d05b1468
Carlo Cervellin <carlocervellin@gmail.com>
2019-01-23 22:54:36 +0100

    🎨 SF

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const OAuth = require('oauth');
-const url = require('url');
+const OAuth = require("oauth");
+const url = require("url");
 
 module.exports = (req, res) => {
-	const header = {
-		'Yahoo-App-Id': process.env.YAHOO_APP_ID
-	};
-	const requestOrigin = req.headers.origin;
-	const { path } = url.parse(req.url);
-	const request = new OAuth.OAuth(
-		null,
-		null,
-		process.env.YAHOO_CONSUMER_KEY,
-		process.env.YAHOO_CONSUMER_SECRET,
-		'1.0',
-		null,
-		'HMAC-SHA1',
-		null,
-		header
-	);
-	request.get(
-		'https://weather-ydn-yql.media.yahoo.com' + path,
-		null,
-		null,
-		function(err, data, result) {
-			if (err) {
-				res.end('ERROR');
-			} else {
-				res.end(data);
-			}
-		}
-	);
+  const header = {
+    "Yahoo-App-Id": process.env.YAHOO_APP_ID
+  };
+  const requestOrigin = req.headers.origin;
+  const { path } = url.parse(req.url);
+  const request = new OAuth.OAuth(
+    null,
+    null,
+    process.env.YAHOO_CONSUMER_KEY,
+    process.env.YAHOO_CONSUMER_SECRET,
+    "1.0",
+    null,
+    "HMAC-SHA1",
+    null,
+    header
+  );
+  request.get(
+    "https://weather-ydn-yql.media.yahoo.com" + path,
+    null,
+    null,
+    function(err, data, result) {
+      if (err) {
+        res.end("ERROR");
+      } else {
+        res.end(data);
+      }
+    }
+  );
 };
